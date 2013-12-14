c3f440dafe75e35f66e2f774271f73fc6f3a6103
unknown <lnagi@dlulnagi11306.sapient.com>
2013-12-14 14:32:38 +0530

    First commit in repo

---
+++ LAKHA/Games/Template/js/CatchEm.js
@@ -0,0 +1,22 @@
+
+var CatchEM = {};
+
+(function(ns){
+
+	//Define Game State
+	ns.state = {
+	
+	};
+	
+	//Define game event-handlers
+	ns.evtH = {
+	
+	};
+	
+	//Define game Function
+	ns.fns = {
+	
+	};
+	
+
+})(catchEM, undefined);
